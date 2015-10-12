593985f15f60b566ad10d37ba7b3fba7e41fd87e
coder sparks <codersparks@gmail.com>
2015-10-12 17:40:05 +0100

    updated to get configserver running

--- coiledcloud-config-server/src/main/java/com/github/codersparks/coiledcloud/services/configserver/Application.java
+++ coiledcloud-config-server/src/main/java/com/github/codersparks/coiledcloud/services/configserver/Application.java
@@ -4,17 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by codersparks on 11/10/2015.
  */
-@SpringBootApplication
+@Configuration
 @EnableAutoConfiguration
 @EnableConfigServer
 public class Application {
 
     public static void main(String[] args) {
-        System.setProperty("spring.config.name", "config-server");
+        System.setProperty("spring.config.name", "configserver");
         SpringApplication.run(Application.class, args);
     }
 }
